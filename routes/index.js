756b56f6c4f8062b10ece25b4bb8cbbe1cdd79ff
DESKTOP-PTK6NLO\Ibrahim Akbar Local <ibrahim.akbar31@gmail.com>
2019-02-18 01:02:02 +0700

    update package
    updating package json

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check'); 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-    res.send('index root')
+router.get('/', async (req, res) => {
+    try {
+        //res.render('index', { title: 'Express' });
+        res.send('index root')
+    } catch (err) {
+        console.log(err)
+        
+    }
 });
 
 module.exports = router;
